f6ba086dbe3a29e48a7f25f61af2b780443b35a8
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-03 14:06:29 +0530

    adding Uc2 check equality

---
+++ UC-2 LineComp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UC_2_LineComp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int x1, y1, x2, y2;
+                double lengthofLine;
+                double lengthofLine1, lengthofLine2;
+                x1 = 2;
+                y1 = 3;
+                x2 = 4;
+                y2 = 5;
+            Console
+                lengthofLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+                Console.WriteLine("Length of line =" + lengthofLine);
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
