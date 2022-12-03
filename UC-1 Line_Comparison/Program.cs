b2c6a7f1906d6db2a48a1d1dde2e36ca7f0b245b
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-03 11:29:40 +0530

    Adding UC-1 LineComp

---
+++ UC-1 Line_Comparison/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UC_1_Line_Comparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {int x1,y1, x2, y2;
+            double lengthofLine;
+            x1 = 2;
+            y1=3;
+            x2 = 4;
+            y2 = 5;
+            lengthofLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of line ="+lengthofLine);
+        }
+    }
+}
